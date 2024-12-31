e5fef27239a64c3733508c6b57f6b706905c2b32
bearcattt <ewigglet8@gmail.com>
2024-12-31 11:54:49 -0500

    Turning password protection off by default

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import type { ConfigTypes } from '@src/types/config';
 const config: ConfigTypes = {
   port: 8080, // The port lunar runs on (Default: 8080)
   auth: {
-    protect: true, // Enable or disable authentication (Default: false)
+    protect: false, // Enable or disable authentication (Default: false)
     log: true, // Logs when a user logs in (Default: true)
     users: [
       // To add more users, follow this format:
